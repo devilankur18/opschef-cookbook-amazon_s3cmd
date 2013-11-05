830c6c5c0d2623fd0b3168f851bd3f671c40e101
Ankur Agarwal <signup.ankur@gmail.com>
2013-11-05 13:34:48 +0530

    Use the default logic for loading the data bag secret

--- recipes/databag_and_config.rb
+++ recipes/databag_and_config.rb
@@ -6,28 +6,25 @@
 #
 
 # Make sure data bag secret exists.
-if File.exists?(node['amazon_s3cmd']['data_bag_secret']) then
-
+begin
   # Set up encrypted data bag.
-  data_bag_secret = node['amazon_s3cmd']['data_bag_secret']
-  s3_secret = Chef::EncryptedDataBagItem.load_secret(data_bag_secret)
   s3_creds = Chef::EncryptedDataBagItem.load(node['amazonZ_s3cmd']['encrypted_data_bag_name'], node['amazonZ_s3cmd']['encrypted_data_bag_item'], s3_secret)
-
-  # Save Amazon key & secret to variables.
-  s3_key = s3_creds["s3_access_key"]
-  s3_secret = s3_creds["s3_secret_key"]
-
-  # Save creds to node.
-  node.set['amazon_key'] = s3_key
-  node.set['amazon_secret'] = s3_secret
-
-  else
-
-    # Inform user to create data bag & item.
-    Chef::Log.info("WARNING: You must create encrypted data bag named s3cmd & item named s3cfg
-                    with data bag secret copied to /etc/chef/encrypted_data_bag_secret on your node
-                    in order to use the amazon_s3cmd cookbook or override via role!! ")
+rescue Exception => e
+  
+  # Inform user to create data bag & item.
+  Chef::Log.info("WARNING: You must create encrypted data bag named s3cmd & item named s3cfg
+                  with data bag secret copied to /etc/chef/encrypted_data_bag_secret on your node
+                  in order to use the amazon_s3cmd cookbook or override via role!! ")
 end
+
+# Save Amazon key & secret to variables.
+s3_key = s3_creds["s3_access_key"]
+s3_secret = s3_creds["s3_secret_key"]
+
+# Save creds to node.
+node.set['amazon_key'] = s3_key
+node.set['amazon_secret'] = s3_secret
+
 
 # Deploy config file for s3cmd.
 template "#{node['etc']['passwd'][node['amazon_s3cmd']['user']]['dir']}/.s3cfg" do
